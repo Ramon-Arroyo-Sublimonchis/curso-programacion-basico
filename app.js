77c74925a4f13453cbacfee1bd24dfdce0a8826c
Sublimonchis <fusionevolutiva@outlook.com>
2018-07-24 17:22:15 -0500

    Listo el ejercicio del ciclo for

--- app.js
+++ app.js
@@ -1,66 +1,15 @@
-/*let nota = 50;
-let notaLetra= "";
 
-if (nota>=90){
-    notaLetra = "A";
-} else if(nota>=80){
-    notaLetra="B";
-} else if(nota>=70){
-    notaLetra="C";
-} else if (nota>=60){
-    notaLetra="D"
-} else{
-    notaLetra="F";
-}
+let base = 10;
 
-console.log(nota + " Es igual a = " + notaLetra);*/
+let limite = 15;
 
 
-/*let dia = 1;
+for (let i = 1; i<=limite; i++){
 
-switch(dia){
+    let resultado = base * i;
 
-    case 1:
-        console.log("Es domingo");
-        break;
-    
-    case 1:
-        console.log("Es Lunes");
-        break;
-
-    case 3:
-        console.log("Es Martes");
-        break;
-    
-    case 4:
-        console.log("Es miercoles");
-        break;
-
-    case 5:
-        console.log("Es Jueves");
-
-    case 6:
-        console.log("Es Viernes");
-        break;
-
-    case 7: 
-        console.log("Es sabado");
-        break;
-
-    default:
-        console.log("No es un dia valido");
-    
+    console.log(base + "x" + i + " = " + resultado);
 
 
-}*/
-
-
-let gasolina = 55;
-
-while(gasolina > 50){
-    gasolina--;
-    console.log("Solo te queda " + gasolina + " %");
 
 }
-
-console.log("Ya no tienes gasolina");
