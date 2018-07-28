69e989863874cb2a393e6143c31f1bd1ba986825
Sublimonchis <fusionevolutiva@outlook.com>
2018-07-28 12:44:08 -0500

    Objetos con el ciclo for

--- app2.js
+++ app2.js
@@ -1,11 +1,8 @@
 
-function describirPersona(p){
+/*function describirPersona(p){
     //console.log(`${p.nombre} tiene una edad de ${p.edad} y mide ${p.estatura}`);
     console.log(p.nombre + " tiene una edad de " + p.edad + " Y mide " + p.estatura);
 }
-
-
-
 
 let persona = {
 
@@ -14,4 +11,31 @@ let persona = {
     estatura: 1.90
 }
  
-describirPersona(persona);
+describirPersona(persona);*/
+
+let persona1 = {
+    nombre: "Ramon",
+    edad: 34
+};
+
+
+let persona2 = {
+    nombre: "Vicky",
+    edad: 24
+};
+
+
+let persona3 = {
+    nombre: "Daniel",
+    edad: 36
+};
+
+let personas = [persona1, persona2, persona3];
+
+
+for(i=0; i<personas.length; i++){
+
+    let persona = personas[i];
+    console.log(`${persona.nombre} -- ${persona.edad}`);
+   
+}
