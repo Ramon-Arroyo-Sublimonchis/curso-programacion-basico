3c316eea2c01b8ba4197667fdf7ce3c0e377ba69
Sublimonchis <fusionevolutiva@outlook.com>
2018-07-31 17:47:09 -0500

    Finalizamos con las clases

--- app3.js
+++ app3.js
@@ -1,8 +1,4 @@
-let carro1 = {
-    marca:"Toyota",
-    tipo: "Sedan",
-    puertas: 3
-};
+
 
 class Carro {
     constructor(marca, tipo, puertas){
@@ -10,11 +6,44 @@ class Carro {
         this.tipo = tipo;
         this.puertas = puertas;
         this.creadoEn = "Mañana";
+        this.encendido = false;
+        this.gasolina = 100;
     }
+
+    encenderCarro(){
+        if(this.encendido){
+            console.error("El carro ya estaba encendido");
+            
+        } else {
+            this.encendido = true;
+            console.log("El carro ya esta encendido");
+        }
+
+        return this;
+    }
+
+    realizarViaje(consumo){
+
+    if(this.encendido){
+        if(consumo > this.gasolina){
+            console.log("No puede realizar este viaje: Gasolina " + this.gasolina);
+
+        }else{
+
+        this.gasolina = this.gasolina - consumo;
+        return "Te queda " + this.gasolina;
+
+        } 
+    } else {
+            console.log("El carro esta apagado");
+        }
+    
+    }
+     
 
 }
 
-let carroNuevo = new Carro("Vocho", "Deportivos", 2);
+let carro = new Carro("Vocho", "Deportivos", 2);
 
-console.log(carro1);
-console.log(carroNuevo);
+console.log(carro);
+
